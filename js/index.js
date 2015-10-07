fc24cf1ab8bb220cad2cce4ad112afa265e002b3
TamsinT <tamsintrickett@hotmail.com>
2015-10-07 20:57:12 +1100

    Got all my Java Script Working (Wed Night)
    Added logo as a SVG just to see. added breakpoints for sizing it.
    
    Got my responsive menus working. Just need to style them.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
 
+  $('#menuButton').on('click', menubuttonhandler);
+
+  function menubuttonhandler(b) {
+    console.log('menu clicked');
+
+     $('.menuItem').toggle(600);
+  }
+
   $(".menuItem").on('click', menuHandler);
 
   function menuHandler(e) {
     console.log('summary shown');
 
+    $('.open .menuContent').toggle(500);
+    $('.open').removeClass('open');
+
     $('.menuContent', e.currentTarget).toggle(500);
+    $(e.currentTarget).addClass('open');
   }
+
+
+$("#signUp").on('click', submitEmail);
+
+    function submitEmail() {
+     console.log('email saved');
+
+     window.alert("Thanks! You are now subscribed to Galleria's Newsletter!")
+
+     /*  $('.thanksAlert').show();  */
+   }
+
+
 
 });
